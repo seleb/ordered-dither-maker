3b491a7a1def82db79a6c622085c72862f57f15f
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-05-09 19:22:08 -0400

    feat: convert dither state to texture

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,6 +2,10 @@ import 'preact';
 import { render } from 'preact';
 import pkg from '../../package.json';
 import { Grid } from './Grid';
+const outputCanvas = document.createElement('canvas');
+const outputCtx = outputCanvas.getContext('2d') as CanvasRenderingContext2D;
+outputCanvas.width = 0;
+outputCanvas.height = 0;
 
 function App() {
 	const [layers, setLayers] = useState(4);
@@ -23,6 +27,25 @@ function App() {
 		dither,
 		layer,
 	]);
+
+	// convert dither state to texture
+	useEffect(() => {
+		const l = dither.length;
+		const h = dither[0].length;
+		const w = dither[0][0].length;
+		outputCanvas.width = w;
+		outputCanvas.height = h;
+		outputCtx.fillStyle = `rgb(0,0,0)`;
+		outputCtx.fillRect(0, 0, w, h);
+		for (let y = 0; y < h; ++y) {
+			for (let x = 0; x < w; ++x) {
+				const brightness = Math.max(0, (dither.findIndex(d => d[y][x]) + 1) / (l) * 256 - 1);
+				outputCtx.fillStyle = `rgb(${brightness}, ${brightness}, ${brightness})`;
+				outputCtx.fillRect(x,y,1,1);
+			}
+		}
+		setSrcOutput(outputCanvas.toDataURL());
+	}, [dither]);
 	return (
 		<>
 			<main>
@@ -53,6 +76,13 @@ function App() {
 				/>
 				<label htmlFor="layer">layer:</label>
 				<input id="layer" type="range" min={0} max={layers - 1} step={1} value={layer} data-value={layer + 1} onChange={useRange(setLayer)} />
+
+				<hr />
+
+				<figure>
+					<figcaption>output</figcaption>
+					<img alt="Output image" id="output-img" src={srcOutput} ref={refImgOutput} />
+				</figure>
 			</main>
 			<footer>v{pkg.version}</footer>
 		</>
