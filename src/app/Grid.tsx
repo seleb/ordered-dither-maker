58df3894e7cc95e50fbf978e45cc3e408946e0f0
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-05-11 03:09:50 -0400

    fix: drag to draw not working in firefox

--- src/app/Grid.tsx
+++ src/app/Grid.tsx
@@ -20,7 +20,7 @@ export function Grid({
 			drawing = event.currentTarget;
 			toggleValue(...getPos(target));
 			function onMove(e: MouseEvent) {
-				if (!e.target?.ariaLabel?.startsWith('pixel x')) return;
+				if (e.target?.dataset?.pixel === undefined) return;
 				if (e.target.value === targetValue) return;
 				toggleValue(...getPos(e.target));
 			}
@@ -46,7 +46,7 @@ export function Grid({
 		<div {...props} className="grid">
 			{value.map((row, y) =>
 				row.map((i, x) => (
-					<button aria-label={`pixel x: ${x}, y: ${y}`} data-x={x} data-y={y} disabled={required[y][x]} value={(required[y][x] || value[y][x]).toString()} onMouseDown={startDrawing} onClick={onClick}/>
+					<button data-pixel aria-label={`pixel x: ${x}, y: ${y}`} data-x={x} data-y={y} disabled={required[y][x]} value={(required[y][x] || value[y][x]).toString()} onMouseDown={startDrawing} onClick={onClick}/>
 				))
 			)}
 		</div>
